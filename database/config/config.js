4eb47f1cfa89a53415ffe503752d61b12c446936
giovanni760 <giovanni162005@gmail.com>
2025-04-16 11:35:08 -0600

    update 3

--- database/config/config.js
+++ database/config/config.js
@@ -1,18 +1,23 @@
-require('dotenv').config()
-
-module.exports = {
- development:{
-        url: process.env.DEV_DATABASE_URL,
-        dialect: 'mysql',
-        },
-        test: {
-
-        url: process.env.TEST_DATABASE_URL,
-        dialect: 'mysql',
-        },
-        production: {
-        url: process.env.DATABASE_URL,
-        dialect: 'mysql',
-        },
+{
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "database_development",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
 }
-
